40013173990f18d8e60e74621f5053a3cfa17906
12moi <moi.shadrack@student.moringaschool.com>
2022-03-05 11:02:44 +0300

    add class to form.py file for user profile and commet forms

---
+++ app/main/form.py
@@ -0,0 +1,16 @@
+from turtle import title
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    title=StringField('Title',validators=[Required()])
+    
+
+class UpdateProfile(FlaskForm):
+       bio=TextAreaField('Write a brief bio about you.... ',validators=[Required()])
+       submit=SubmitField('save')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[Required()])
+    submit = SubmitField('Comment')
