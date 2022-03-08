3d02f8e8c70fa2402fc5be1d5e5565b28c0bea9d
12moi <moi.shadrack@student.moringaschool.com>
2022-03-08 11:17:10 +0300

    add Procfile

--- manage.py
+++ manage.py
@@ -4,14 +4,15 @@ from app import create_app,db
 from app.models import User, Pitch,Upvote,Downvote,Comment
 from  flask_migrate import Migrate,MigrateCommand
 
+
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
-
 migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
+
+manager.add_command('db',MigrateCommand)
+manager.add_command('run',Server(use_debugger=True))
 
 @manager.command
 def test():
