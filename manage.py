ac73c253ce0b8610ffb8be2c4f6bcb3a1eae1164
12moi <moi.shadrack@student.moringaschool.com>
2022-03-08 19:36:34 +0300

    add start.sh file

--- manage.py
+++ manage.py
@@ -1,18 +1,17 @@
-# from app import create_app,db
+
 from flask_script import Manager, Server
 from app import create_app,db
 from app.models import User, Pitch,Upvote,Downvote,Comment
-from  flask_migrate import Migrate,MigrateCommand
-
+from flask_migrate import Migrate
+from  flask_migrate import MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
-migrate = Migrate(app,db)
+manager.add_command('server',Server)
 
-
+Migrate=Migrate(app,db)
 manager.add_command('db',MigrateCommand)
-manager.add_command('run',Server(use_debugger=True))
 
 @manager.command
 def test():
